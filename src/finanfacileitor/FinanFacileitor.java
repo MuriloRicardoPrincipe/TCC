9a930460fdd89d97fd0e077f5b203e70ce0b0d2a
Murilo <murilo.rprincipe@hotmail.com>
2022-07-22 15:33:45 -0300

    Tcc

---
+++ src/finanfacileitor/FinanFacileitor.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package finanfacileitor;
+
+/**
+ *
+ * @author muril
+ */
+public class FinanFacileitor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        TelaControle tela = new TelaControle();
+        tela.setVisible(true);
+        tela.setTitle("FinanFacileitor");
+    }
+    
+}
